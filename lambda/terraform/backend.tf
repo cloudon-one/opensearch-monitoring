6a3149fd22483298347f54a74f4304a18c6e9911
Yaar <74919751+yaroslavopsguru@users.noreply.github.com>
2024-11-12 10:41:14 +0200

    clean up

--- lambda/terraform/backend.tf
+++ lambda/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "kenbi-dev-terraform-tf-tfstates"
+    bucket = "dev-terraform-tf-tfstates"
     key    = "terraform/dev/opensearch-monitoring/lambda/terraform/terraform.tfstate"
     region = "eu-west-1"
   }
