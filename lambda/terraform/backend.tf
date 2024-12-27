97a9272e2f88317ca67c213225b710ee040f6606
Yaar <74919751+yaroslavopsguru@users.noreply.github.com>
2024-12-27 13:50:47 +0200

    Update backend.tf
    Signed-off-by: Yaar <74919751+yaroslavopsguru@users.noreply.github.com>

--- lambda/terraform/backend.tf
+++ lambda/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cloudon-aws-admin-tf-state-010"
-    key    = "opensearch-monitoring/lambda/terraform/terraform.tfstate"
+    bucket = ""
+    key    = "terraform/terraform.tfstate"
     region = "us-east-2"
   }
 }
